3c911267e0f3d25044da53a01d97d10125447520
Korlall <52757669+Korlall@users.noreply.github.com>
2023-03-07 13:39:32 -0500

    Implanté gestion des erreurs des urls

--- import.py
+++ import.py
@@ -7,11 +7,15 @@ import unicodedata
 open_quizz_db_data = (
     ("Animaux", "Les chats", "https://www.codeavecjonathan.com/res/mission/openquizzdb_50.json"),
     ("Arts", "Musée du Louvre", "https://www.codeavecjonathan.com/res/mission/openquizzdb_86.json"),
+    ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627384/OpenQuizzDB_124/openquizzdb_124.json"),
     ("Cinéma", "Alien", "https://www.codeavecjonathan.com/res/mission/openquizzdb_241.json"),
     ("Cinéma", "Star wars", "https://www.codeavecjonathan.com/res/mission/openquizzdb_90.json"),
 )
-# Lien pas bon
-# ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627384/OpenQuizzDB_124/openquizzdb_124.json")
+
+
+# https://www.codeavecjonathan.com/res/mission/openquizzdb_86.json
+# https://www.kiwime.com/oqdb/files/2124627384/OpenQuizzDB_124/openquizzdb_124.json
+# https://www.codeavecjonathan.com/res/mission/openquizzdb_124.json
 
 
 def strip_accents(s):
@@ -19,15 +23,22 @@ def strip_accents(s):
 
 
 def get_quizz_filename(categorie, titre, difficulte):
-    return strip_accents(categorie).lower().replace(" ", "") + "_" + strip_accents(titre).lower().replace(" ", "") + "_" + strip_accents(difficulte).lower().replace(" ", "") + ".json"
+    return strip_accents(categorie).lower().replace(" ", "") + "_" + strip_accents(titre).lower().replace(" ",
+                                                                                                          "") + "_" + strip_accents(
+        difficulte).lower().replace(" ", "") + ".json"
 
 
 def generate_json_file(categorie, titre, url):
     out_questionnaire_data = {"categorie": categorie, "titre": titre, "questions": []}
     out_questions_data = []
     response = requests.get(url)
-    data = json.loads(response.text)
-    all_quizz = data["quizz"]["fr"]
+    try:
+        data = json.loads(response.text)
+    except json.JSONDecodeError:
+        print(f"ERROR: url {url} invalide.")
+        return
+    else:
+        all_quizz = data["quizz"]["fr"]
     for quizz_title, quizz_data in all_quizz.items():
         out_filename = get_quizz_filename(categorie, titre, quizz_title)
         print(out_filename)
@@ -37,7 +48,7 @@ def generate_json_file(categorie, titre, url):
             question_dict["titre"] = question["question"]
             question_dict["choix"] = []
             for ch in question["propositions"]:
-                question_dict["choix"].append((ch, ch==question["réponse"]))
+                question_dict["choix"].append((ch, ch == question["réponse"]))
             out_questions_data.append(question_dict)
         out_questionnaire_data["questions"] = out_questions_data
         out_json = json.dumps(out_questionnaire_data)
@@ -50,4 +61,3 @@ def generate_json_file(categorie, titre, url):
 
 for quizz_data in open_quizz_db_data:
     generate_json_file(quizz_data[0], quizz_data[1], quizz_data[2])
-
